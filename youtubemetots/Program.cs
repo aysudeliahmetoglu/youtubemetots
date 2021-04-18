9087aa6afd5893bcba5f3ac9931a73a1c7fc365a
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2021-04-19 01:04:57 +0300

    Add project files.

---
+++ youtubemetots/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace youtubemetots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string yazilacakmetin = "merhaba türkiyem ";
+            //metinyaz(yazilacakmetin);
+
+            int sonuc = topla(5,15);
+            Console.Write("TOPLAMA SONUCU:{0}", sonuc);
+            Console.ReadKey();
+        }
+
+        //public static void metinyaz (string metin)
+        //  {
+        //    for (int i = 1; i <=5; i++)
+        //    {
+        //        Console.WriteLine(metin);
+        //    }
+        //  }
+
+        private static int topla(int s1,int s2)
+        {
+            return (s1+s2);
+        }
+    }
+}
